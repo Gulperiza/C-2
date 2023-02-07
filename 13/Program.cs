73154ae4a819db8de70ae614bc42abb781ef8693
Gulperiza <gulperiza96@gmail.com>
2023-02-07 21:37:24 +0500

    Конечный

---
+++ 13/Program.cs
@@ -0,0 +1,4 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+int a=Convert.ToInt32(Console.ReadLine());
+if (a>99) System.Console.WriteLine($"{a/100%10}");
+if (a<=99) System.Console.WriteLine("Нет третий цифры");
---
+++ 15/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+System.Console.WriteLine("Введите число:");
+double a=Convert.ToDouble(Console.ReadLine());
+if(a==1) System.Console.WriteLine($"Сегодня ПОНЕДЕЛЬНИК, а значит не выходной");
+if(a==2) System.Console.WriteLine($"Сегодня ВТОРНИК, а значит не выходной");
+if(a==3) System.Console.WriteLine($"Сегодня СРЕДА, а значит не выходной");
+if(a==4) System.Console.WriteLine($"Сегодня ЧЕТВЕРГ, а значит не выходной");
+if(a==5) System.Console.WriteLine($"Сегодня ПЯТНИЦА, а значит не выходной");
+if(a==6) System.Console.WriteLine($"Сегодня СУББОТА, а значит выходной");
+if(a==7) System.Console.WriteLine($"Сегодня ВОСКРЕСЕНЬЕ,а значит выходной");
